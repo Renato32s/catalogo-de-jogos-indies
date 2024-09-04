f664d7e10bf14dea1df550f9c6ba36a91b814a1b
Renato32s <sous462@gmail.com>
2024-09-04 12:58:59 -0300

    04/09/24
    Realizando algumas modificações e adicionando mais conteúdos!

---
+++ scripts/dados.js
@@ -0,0 +1,27 @@
+let dados = [
+    {
+        titulo: 'Greak: Memories of Azur',
+        descricao: 'Greak: Memories of Azur é um jogo indie de aventura que te coloca no controle de três irmãos: Greak, Adara e Raydel. Em um mundo devastado por uma invasão, você precisará usar as habilidades únicas de cada personagem para superar desafios, resolver puzzles e construir um dirigível para escapar. O jogo se destaca por seus gráficos desenhados à mão, sua trilha sonora envolvente e sua narrativa emocionante, que te convida a explorar um mundo rico e cheio de detalhes. Se você gosta de jogos de plataforma com elementos de Metroidvania, Greak é uma ótima opção!',
+        link: 'https://navegante-entertainment.com/'
+    },
+    {
+        titulo: 'Hollow Knight',
+        descricao: 'Hollow Knight é um aclamado jogo indie de metroidvania que te leva para um reino subterrâneo em ruínas, Hallownest. Assuma o papel de um pequeno inseto cavaleiro e explore um mundo intricado repleto de criaturas perigosas e segredos obscuros. Desvende os mistérios de Hallownest, domine habilidades desafiadoras e enfrente chefes épicos. Com gráficos detalhados em pixel art, uma trilha sonora atmosférica e uma trilha sonora atmosférica e uma jogabilidade profunda, Hollow Knight oferece uma experiência memorável para os fãs do gênero.',
+        link: 'https://www.hollowknight.com/'
+    },
+    {
+        titulo: 'Celeste',
+        descricao: 'Celeste é um jogo indie de plataforma 2D com foco em mecânicas desafiadoras e uma história comovente. Controlando Madeline, você precisará escalar montanhas perigosas, superando seus próprios limites e enfrentando seus medos. O jogo se destaca por sua jogabilidade precisa, sua trilha sonora vibrante e sua narrativa sobre saúde mental. Celeste é uma experiência emocionante que te desafiará tanto física quanto emocionalmente.',
+        link: 'https://www.celeste.game/'
+    },
+    {
+        titulo: 'Dead Cells',
+        descricao: 'Dead Cells é um rogue-lite de ação em 2D que te coloca no papel de um prisioneiro imortal em um castelo amaldiçoado. Explore um labirinto de salas geradas proceduralmente, lute contra inimigos implacáveis e descubra novas habilidades e armas a cada partida. Com um combate frenético, gráficos pixel art vibrantes e uma trilha sonora contagiante, Dead Cells oferece uma experiência viciante e desafiadora.',
+        link: 'https://deadcells.com/'
+    },
+    {
+        title: 'Ori and the Blind Forest',
+        description: 'Ori and the Blind Forest é uma plataforma de metroidvania com belos gráficos pintados à mão e uma trilha sonora emocionante. Assuma o papel de Ori, uma pequena criatura espiritual, e explore uma floresta mágica em busca de seu lar. Dominando habilidades como correr pelas paredes, deslizar e usar magia, você enfrentará desafios emocionantes e desvendará os mistérios da floresta.',
+        link: 'https://www.microsoft.com/en-us/store/p/ori-and-the-blind-forest/9nbltgh4ms3c'
+    }
+];
